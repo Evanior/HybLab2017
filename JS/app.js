c5ae63f8695d6249938ed99577e9cbe1195bc762
Huard Timothée <huard.tim@laposte.net>
2017-03-29 13:17:53 +0200

    ajout de odometer

--- JS/app.js
+++ JS/app.js
@@ -6,4 +6,6 @@ function setUp(){
 	particlesJS.load('particles-js', 'assets/particles.json', function() {
 	  console.log('callback - particles.js config loaded');
 	});
+	var odometer = new Odometer({ el: $('.odometer')[0], value: 123, theme: 'train-station' });
+	odometer.render();
 }
