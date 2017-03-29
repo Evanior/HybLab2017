f2f1e739ae16edfe40bb83fa500bd8e8b3d6acbd
Dimitri <dimtri.ferron@gmail.com>
2017-03-29 16:05:08 +0200

    overlay finish

--- JS/app.js
+++ JS/app.js
@@ -9,8 +9,8 @@ $(document).ready(function(){
 
 function typed_text(){
   $("#typed_text").typed({
-    strings: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
-    typeSpeed: 10, callback: function(){
+    strings: ["Là tu te demande sur quoi tu es tombé, on te dit “Smart City” tu vois des hoverboards et des chaussures qui se lacent toutes seules. On te dit “Ville intelligente” tu trouve ça tout de suite un peu moins stylé. Mais nous on va te dire “Ville maline” et là tu pense présent, tu pense collaboratif, environnement, pratique, économique. Et si t’y pensais pas, maintenant c’est le cas ! Alors laisse toi guider au fil du temps et découvre comment en étant malin ta ville le deviendra aussi !"],
+    typeSpeed: 1, callback: function(){
       $('.typed-cursor').hide();}
   });
 }
@@ -26,6 +26,6 @@ function setUp(){
 
   // typed content overlay
   $("#typed_title").typed({strings: ["Qu'est ce que smart city ?"],
-    typeSpeed: 30, callback: function(){typed_text();}
+    typeSpeed: 30, callback: function(){$('.typed-cursor').first().hide();typed_text();}
   });
 };
