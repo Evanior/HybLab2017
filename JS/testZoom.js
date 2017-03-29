cbc0a510c735f2ce1e4fca0b32586b276bb7a2d7
Huard Timothée <huard.tim@laposte.net>
2017-03-29 12:03:57 +0200

    test de zoomooz

---
+++ JS/testZoom.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	setUp();
+});
+
+function setUp(){
+	$("#btn1").click(function(event) {
+
+	});
+	$("#btn2").click(function(event) {
+
+	});
+	$("#btn3").click(function(event) {
+
+	});
+}
