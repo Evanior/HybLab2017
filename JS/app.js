a2b1e466422450b8a4fc25f498a08512cfcee53e
Huard Timothée <huard.tim@laposte.net>
2017-03-29 14:25:54 +0200

    deplacement de la div de particule

--- JS/app.js
+++ JS/app.js
@@ -3,9 +3,12 @@ $(document).ready(function() {
 });
 
 function setUp(){
+	//particule js
 	particlesJS.load('particles-js', 'assets/particles.json', function() {
 	  console.log('callback - particles.js config loaded');
 	});
+
+	//compter metro
 	var odometer = new Odometer({ el: $('.odometer')[0], value: 123, theme: 'train-station' });
 	odometer.render();
 }
