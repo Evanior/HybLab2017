288c8d0e09185df753233ec991bb4f11f9a7f69f
Dimitri <dimtri.ferron@gmail.com>
2017-03-29 14:36:15 +0200

    overlay

--- JS/app.js
+++ JS/app.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
   setUp();
-  typed_title();
   $('.overlay .overlay_content .skip').click(function(event){
     console.log('test');
     event.preventDefault();
@@ -8,22 +7,13 @@ $(document).ready(function(){
   });
 });
 
-function typed_title(){
-    $("#typed_title").typed({
-  strings: ["Qu'est ce que smart city ?"],
-  typeSpeed: 30, callback: function(){
-    typed_text();
-    }
-  });
-}
 function typed_text(){
   $("#typed_text").typed({
-strings: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
-typeSpeed: 10, callback: function(){
-    $('.typed-cursor').hide();
-  }
-});
-
+    strings: ["Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."],
+    typeSpeed: 10, callback: function(){
+      $('.typed-cursor').hide();}
+  });
+}
 function setUp(){
 	//particule js
 	particlesJS.load('particles-js', 'assets/particles.json', function() {
@@ -33,4 +23,9 @@ function setUp(){
 	//compter metro
 	var odometer = new Odometer({ el: $('.odometer')[0], value: 123, theme: 'train-station' });
 	odometer.render();
-}
+
+  // typed content overlay
+  $("#typed_title").typed({strings: ["Qu'est ce que smart city ?"],
+    typeSpeed: 30, callback: function(){typed_text();}
+  });
+};
